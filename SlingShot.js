b918c561538b2a9d23452ce08ac0208d54b01786
Arthur-Clarke <arthurclarque@gmail.com>
2021-12-04 12:25:27 -0300

    estilingue pra jogar passaros

--- SlingShot.js
+++ SlingShot.js
@@ -10,6 +10,7 @@ class SlingShot{
         this.chain = Constraint.create(options);
         this.sling1 = loadImage("./sprites/sling1.png");
         this.sling2 = loadImage("./sprites/sling2.png");
+        this.sling3 = loadImage("./sprites/sling3.png");
         World.add(world, this.chain);
     }
     display(){
@@ -18,8 +19,15 @@ class SlingShot{
         if(this.chain.bodyA){
             const positionA = this.chain.bodyA.position;
             const positionB = this.pointB;
-            strokeWeight(3);
+            push();
+            strokeWeight(10);
             // line(positionA.x,positionA.y,positionB.x,positionB.y);
+            stroke(67,31,12);
+            line(positionA.x+20,positionA.y,positionB.x+20,positionB.y);
+            stroke(48,22,8);
+            line(positionA.x-25,positionA.y,positionB.x-25,positionB.y);
+            image(this.sling3, positionA.x-30,positionA.y-10, 15, 30);
+            pop();
         }
         
     }
